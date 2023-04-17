c9af85e57b7bc7953e7b014ab5b5428909609ce8
Murilo Ferreira <murilo@jogoglobal.com>
2023-04-17 20:07:59 -0300

    Changes ToDo

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,19 +1,24 @@
 <template>
   <v-card>
-    <v-card-title>ToDo List</v-card-title>
+    <v-card-title>ToDo</v-card-title>
     <v-card-text>
-      <v-text-field label="Tarefa" v-model="newTask" @keyup.enter="addTask" />
+      <v-text-field
+        v-model="newTask"
+        label="Nova tarefa"
+        @keyup.enter="addTask"
+      ></v-text-field>
       <v-list>
-        <v-list-item
-          v-for="(task, index) in tasks"
-          :key="index"
-          class="d-flex justify-space-between align-center"
-        >
-          <v-list-item-content>
-            <v-checkbox v-model="task.completed" color="primary" />
-            {{ task.description }}
-          </v-list-item-content>
-          <v-btn color="error" @click="removeTask(index)">Remover</v-btn>
+        <v-list-item v-for="(task, index) in tasks" :key="index">
+          <v-checkbox
+            v-model="task.done"
+            @change="updateTask(index, task)"
+          ></v-checkbox>
+          <v-list-item-title :class="{ 'task-done': task.done }">{{
+            task.text
+          }}</v-list-item-title>
+          <v-btn icon @click="deleteTask(index)">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
         </v-list-item>
       </v-list>
     </v-card-text>
@@ -24,39 +29,42 @@
 import { defineComponent, ref } from "vue";
 
 interface Task {
-  description: string;
-  completed: boolean;
-}
-
-const newTask = ref("");
-const tasks = ref<Task[]>([]);
-
-function addTask() {
-  if (newTask.value.trim() !== "") {
-    tasks.value.push({ description: newTask.value, completed: false });
-    newTask.value = "";
-  }
-}
-
-function removeTask(index: number) {
-  tasks.value.splice(index, 1);
+  text: string;
+  done: boolean;
 }
 
 export default defineComponent({
   setup() {
+    const tasks = ref<Task[]>([{ text: "Exemplo de tarefa", done: false }]);
+    const newTask = ref("");
+
+    function addTask() {
+      if (newTask.value.trim() !== "") {
+        tasks.value.push({ text: newTask.value.trim(), done: false });
+        newTask.value = "";
+      }
+    }
+
+    function deleteTask(index: number) {
+      tasks.value.splice(index, 1);
+    }
+
+    function updateTask(index: number, updatedTask: Task) {
+      tasks.value[index] = updatedTask;
+    }
+
     return {
-      newTask,
       tasks,
+      newTask,
       addTask,
-      removeTask,
+      deleteTask,
+      updateTask,
     };
   },
 });
 </script>
-
 <style scoped>
-.v-list-item-content {
-  display: flex;
-  align-items: center;
+.task-done {
+  text-decoration: line-through;
 }
 </style>
